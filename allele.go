41609600c8666d7837a558410b7dfc28bcdf0eab
Ryan J. O'Neil <ryanjoneil@gmail.com>
2013-07-24 10:07:57 -0400

    starting to add gene tests

--- allele.go
+++ allele.go
@@ -2,21 +2,25 @@ package govolve
 
 import "math"
 
+// TODO: Other types of alleles
+
 // Operators have access to the data they are operating on as
 // well as the Allele Stack for evaluation of chromosomes.
 type Operator func([]float64, *Stack) float64
 
-// An Allele represents a single operation that can occur against a stack.Stack.
+// An Allele represents a single operation that can occur against a Stack.
 type Allele struct {
 	Sigil string
 	Arity int
 	Op    Operator
 }
 
+// Generates an Allele that simply returns the index of a data slice.
 func NewIndexAllele(sigil string, index int) *Allele {
 	return &Allele{sigil, 0, func(d []float64, s *Stack) float64 { return d[index] }}
 }
 
+// Generates an allele that simply returns a given value.
 func NewValueAllele(value float64) *Allele {
 	return &Allele{"", 0, func(d []float64, s *Stack) float64 { return value }}
 }
@@ -29,6 +33,3 @@ var ArithmeticAlleles = []*Allele{
 	&Allele{"/", 2, func(d []float64, s *Stack) float64 { return s.Pop().Op(d, s) / s.Pop().Op(d, s) }},
 	&Allele{"^", 2, func(d []float64, s *Stack) float64 { return math.Pow(s.Pop().Op(d, s), s.Pop().Op(d, s)) }},
 }
-
-// TODO: 1/0, etc
-// TODO: trig alleles, etc
--- gene.go
+++ gene.go
@@ -1,5 +1,9 @@
 package govolve
 
+import "strings"
+
+var nextId = 0
+
 // A Gene is a series of Alleles that can be evaluated against a given data array.
 // It contains a Head, which consists of both Operators and Indexes, and a Tail, which
 // is merely Indexes. This is so, if the Head contains only Operators of maximum Arity,
@@ -7,28 +11,50 @@ package govolve
 // valid. Note this does not imply it is semantically valid. Genes are evaluated
 // using prefix notation: +-a+bcd = (+ (- a (+ b c)) d) = (a - (b + c)) + d.
 type Gene struct {
-	//Id      int
+	Id      int
 	Alleles []*Allele
-	//Head    []Allele
+	Coding  int
+}
+
+// Creates a Gene type from a slice of Alleles.
+func NewGene(a []*Allele) *Gene {
+	g := &Gene{Id: nextId, Alleles: a}
+	g.Coding = g.CodingIndex()
+	nextId += 1
+	return g
+}
+
+// Converts a Gene to its string representation.
+func (g *Gene) String() string {
+	// TODO: probably a more idiomatic way to do this...
+	s := make([]string, len(g.Alleles))
+	for i, a := range g.Alleles {
+		s[i] = a.Sigil
+	}
+	return strings.Join(s, "")
 }
 
 // Finds the last index that is part of a Gene's phenotype. This is necessary
-// to evaluate the Gene, since all of its alleles may not be used.
-func (c *Gene) LastAlleleIndex() int {
+// to evaluate the Gene, since all of its alleles may not be used. Otherwise
+// called the coding region of the Gene.
+func (g *Gene) CodingIndex() int {
 	arity := 1
-	for i, a := range c.Alleles {
+	for i, a := range g.Alleles {
 		arity += a.Arity - 1
 		if arity < 1 {
 			return i
 		}
 	}
-	return len(c.Alleles) - 1
+	return len(g.Alleles) - 1
 }
 
-func (c *Gene) Eval(data []float64) float64 {
+// Evaluates a given Gene against some set of data.
+func (g *Gene) Eval(data []float64) float64 {
+	// Genes are prefix notation, so we simply start at the last allele
+	// in the coding region of the Gene and evaluate backward.
 	var stack Stack
-	for i := c.LastAlleleIndex(); i >= 0; i-- {
-		allele := c.Alleles[i]
+	for i := g.Coding; i >= 0; i-- {
+		allele := g.Alleles[i]
 		if allele.Arity > 0 {
 			f := allele.Op(data, &stack)
 			a := NewValueAllele(f)
@@ -39,5 +65,3 @@ func (c *Gene) Eval(data []float64) float64 {
 	}
 	return stack.Pop().Op(data, &stack)
 }
-
-// TODO: func RandomGene(headLength int, tailLength int, )
